19c262fb8225e57da5310cf6dbb43a799df74356
ericabirgit <erl00019@laurea.fi>
2025-05-21 17:21:40 +0300

    Update hirsisanapuu.py
    kommenttei

--- hirsisanapuu.py
+++ hirsisanapuu.py
@@ -1,13 +1,13 @@
 def hirsipuu():
-    sana = "kultainennoutaja"
-    arvattu = []
-    yritykset = 10
+    sana = "kultainennoutaja"    # Arvattava sana
+    arvattu = []                # Lista arvatuista kirjaimista
+    yritykset = 10            # Käytettävissä olevien arvauksien määrä
 
     print("Tervetuloa hirsipuupeliin!")
-    print("_ " * len(sana))
-
+    print("_ " * len(sana))            # Tulostetaan aluksi alaviivat sanan pituuden verran
+# Pelisilmukka, jatkuu niin kauan kuin on yrityksiä jäljellä
     while yritykset > 0:
-        arvaus = input("Arvaa kirjain: ").lower()
+        arvaus = input("Arvaa kirjain: ").lower()    # Käyttäjältä kirjain, muutetaan pieneksi
 
         if not arvaus.isalpha() or len(arvaus) != 1:
             print("Anna yksi kirjain.")
